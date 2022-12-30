b24a41d885296c20419f7ec6caeb53f4a5a25f2b
ChoiHyeonSeong <huns1013@gmail.com>
2022-12-30 17:45:38 +0900

    commit

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ class CustomDataset(torch.utils.data.Dataset):
 __init__(self) : 필요한 변수들을 선언하는 메서드. input으로 오는 x와 y를 load 하거나, 파일목록을 load한다.
 __len__(self) : x나 y 는 길이를 넘겨주는 메서드.
 __getitem__(self, index) : index번째 데이터를 return 하는 메서드.
-
 '''
 class CustomDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels=None):
@@ -60,3 +59,48 @@ class CustomDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.encodings["input_ids"])
+
+'''
+'''
+# Define trainer
+class CustomTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        # forward pass
+        labels = inputs.pop("labels").to(torch.int64)
+
+        type_logit, polarity_logit, tense_logit, certainty_logit = model(**inputs)
+
+        # # simple loss
+        # criterion = {
+        #     'type' : nn.CrossEntropyLoss().to(device),
+        #     'polarity' : nn.CrossEntropyLoss().to(device),
+        #     'tense' : nn.CrossEntropyLoss().to(device),
+        #     'certainty' : nn.CrossEntropyLoss().to(device)
+        # }
+        # loss = criterion['type'](type_logit, labels[::, 0]) + \
+        #             criterion['polarity'](polarity_logit, labels[::, 1]) + \
+        #             criterion['tense'](tense_logit,labels[::, 2]) + \
+        #             criterion['certainty'](certainty_logit, labels[::, 3])
+
+        # focal loss
+        criterion = {
+            'type': FocalLoss().to(device),
+            'polarity': FocalLoss().to(device),
+            'tense': FocalLoss().to(device),
+            'certainty': FocalLoss().to(device)
+        }
+        # labels = labels.type(torch.float).clone().detach()
+        loss = criterion['type'](type_logit, labels[::, 0]) + \
+               criterion['polarity'](polarity_logit, labels[::, 1]) + \
+               criterion['tense'](tense_logit, labels[::, 2]) + \
+               criterion['certainty'](certainty_logit, labels[::, 3])
+
+        outputs = None, \
+                  torch.argmax(type_logit, dim=1), \
+                  torch.argmax(polarity_logit, dim=1), \
+                  torch.argmax(tense_logit, dim=1), \
+                  torch.argmax(certainty_logit, dim=1)
+        return (loss, outputs) if return_outputs else loss
